9d9ba8b384d6add5dcc3bd2d75c5795427a9b804
NemontovMaksim <149705847+NemontovMaksim@users.noreply.github.com>
2024-04-18 20:16:53 +0300

    Дз 2 недели

---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+const gameRouteController = require ("./game");
+const mainRouteController = require ("./main");
+const defaultRouteController = require ("./default");
+const voteRouteController = require ("./vote");
+
+module.exports = {
+    
+    mainRouteController,
+    gameRouteController,
+    defaultRouteController,
+    voteRouteController
+
+}
